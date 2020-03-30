6be7cfe0b2fc6de4830af1878f90dee5e6115b8e
write <writepro4@naver.com>
2020-03-30 11:02:13 +0900

    [update] image_view.js 이미지미리보기 js파일 코드 알아보기 쉽게 수정

--- admin/js/image_view.js
+++ admin/js/image_view.js
@@ -6,21 +6,21 @@ function previewImage(targetObj, View_area) {
     if (ua.indexOf("MSIE") > -1) {
         targetObj.select();
         try {
-            var src = document.selection.createRange().text; // get file full path(IE9, IE10에서 사용 불가)
-            var ie_preview_error = document.getElementById("ie_preview_error_" + View_area);
+            let src = document.selection.createRange().text; // get file full path(IE9, IE10에서 사용 불가)
+            let ie_preview_error = document.getElementById("ie_preview_error_" + View_area);
 
 
             if (ie_preview_error) {
                 preview.removeChild(ie_preview_error); //error가 있으면 delete
             }
 
-            var img = document.getElementById(View_area); //이미지가 뿌려질 곳
+            let img = document.getElementById(View_area); //이미지가 뿌려질 곳
 
             //이미지 로딩, sizingMethod는 div에 맞춰서 사이즈를 자동조절 하는 역할
             img.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+src+"', sizingMethod='scale')";
         } catch (e) {
             if (!document.getElementById("ie_preview_error_" + View_area)) {
-                var info = document.createElement("<p>");
+                let info = document.createElement("<p>");
                 info.id = "ie_preview_error_" + View_area;
                 info.innerHTML = e.name;
                 preview.insertBefore(info, null);
@@ -29,16 +29,16 @@ function previewImage(targetObj, View_area) {
         //ie가 아닐때(크롬, 사파리, FF)
     } else {
         var files = targetObj.files;
-        for ( var i = 0; i < files.length; i++) {
-            var file = files[i];
-            var imageType = /image.*/; //이미지 파일일경우만.. 뿌려준다.
+        for ( let i = 0; i < files.length; i++) {
+            let file = files[i];
+            let imageType = /image.*/; //이미지 파일일경우만.. 뿌려준다.
             if (!file.type.match(imageType))
                 continue;
-            var prevImg = document.getElementById("prev_" + View_area); //이전에 미리보기가 있다면 삭제
+            let prevImg = document.getElementById("prev_" + View_area); //이전에 미리보기가 있다면 삭제
             if (prevImg) {
                 preview.removeChild(prevImg);
             }
-            var img = document.createElement("img");
+            let img = document.createElement("img");
             img.id = "prev_" + View_area;
             img.classList.add("obj");
             img.file = file;
@@ -46,7 +46,7 @@ function previewImage(targetObj, View_area) {
             img.style.height = '300px';
             preview.appendChild(img);
             if (window.FileReader) { // FireFox, Chrome, Opera 확인.
-                var reader = new FileReader();
+                let reader = new FileReader();
                 reader.onloadend = (function(aImg) {
                     return function(e) {
                         aImg.src = e.target.result;
@@ -57,7 +57,7 @@ function previewImage(targetObj, View_area) {
                 //alert('not supported FileReader');
                 if (!document.getElementById("sfr_preview_error_"
                     + View_area)) {
-                    var info = document.createElement("p");
+                    let info = document.createElement("p");
                     info.id = "sfr_preview_error_" + View_area;
                     info.innerHTML = "not supported FileReader";
                     preview.insertBefore(info, null);
@@ -66,14 +66,14 @@ function previewImage(targetObj, View_area) {
         }
     }
 }
-var file = document.querySelector('#questImage');
-var result;
+let file = document.querySelector('#questImage');
+let result;
 
 // 정상 로드시 result에 인코딩 값을 저장하기
-var reader = new FileReader(file);
+let reader = new FileReader(file);
 reader.onload = function() {
     result = reader.result;
-}
+};
 
 // 실패할 경우 에러 출력하기
 reader.onerror = function (error) {
