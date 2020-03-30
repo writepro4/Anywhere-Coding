ef288eb377f84a38bd7ec926f8c5f321dbd01cac
write <writepro4@naver.com>
2020-03-30 11:54:26 +0900

    [fix] cookie.js 쿠키 js파일 주석처리

--- admin/js/cookie.js
+++ admin/js/cookie.js
@@ -1,23 +1,23 @@
-// 쿠키 생성
-function setCookie(cName, cValue, cDay) {
-    let expire = new Date();
-    expire.setDate(expire.getDate() + cDay);
-    cookies = cName + '=' + escape(cValue) + '; path=/ '; // 한글 깨짐을 막기위해 escape(cValue)를 합니다.
-    if (typeof cDay != 'undefined') cookies += ';expires=' + expire.toGMTString() + ';';
-    document.cookie = cookies;
-}
-
-// 쿠키 가져오기
-function getCookie(cName) {
-    cName = cName + '=';
-    let cookieData = document.cookie;
-    let start = cookieData.indexOf(cName);
-    let cValue = '';
-    if (start != -1) {
-        start += cName.length;
-        let end = cookieData.indexOf(';', start);
-        if (end == -1) end = cookieData.length;
-        cValue = cookieData.substring(start, end);
-    }
-    return unescape(cValue);
-}
+// // 쿠키 생성
+// function setCookie(cName, cValue, cDay) {
+//     let expire = new Date();
+//     expire.setDate(expire.getDate() + cDay);
+//     cookies = cName + '=' + escape(cValue) + '; path=/ '; // 한글 깨짐을 막기위해 escape(cValue)를 합니다.
+//     if (typeof cDay != 'undefined') cookies += ';expires=' + expire.toGMTString() + ';';
+//     document.cookie = cookies;
+// }
+//
+// // 쿠키 가져오기
+// function getCookie(cName) {
+//     cName = cName + '=';
+//     let cookieData = document.cookie;
+//     let start = cookieData.indexOf(cName);
+//     let cValue = '';
+//     if (start != -1) {
+//         start += cName.length;
+//         let end = cookieData.indexOf(';', start);
+//         if (end == -1) end = cookieData.length;
+//         cValue = cookieData.substring(start, end);
+//     }
+//     return unescape(cValue);
+// }
