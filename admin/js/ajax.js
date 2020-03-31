074b85e2421e5ab2bac7bfa9d2c722ee5d589b8e
write <writepro4@naver.com>
2020-03-31 10:44:12 +0900

    [update] ajax.js 글내용 삭제시키는 메소드 추가

--- admin/js/ajax.js
+++ admin/js/ajax.js
@@ -9,6 +9,7 @@
 // 6. getCookie 쿠키 가져오기 함수
 // 7. postForm 썸머노트 내용 가져오는 함수
 // 8. writinglist 글 상세 내용 불러오는 함수
+// 9. writingDelete 글 삭제 시키는 함수
 
 
 // 1. 토근 가져오는 함수
@@ -242,6 +243,55 @@ function writinglist() {
             console.log("날짜" + date);
             console.log("본 횃수" + viewCounts);
 
+
+        }
+        //에러 종류 조건문으로 걸러내기
+        , error: function (jqXHR, exception) {
+
+            if (jqXHR.status === 0) {
+                alert('Not connect.\n Verify Network.');
+            } else if (jqXHR.status === 400) {
+                alert('Server understood the request, but request content was invalid. [400]');
+            } else if (jqXHR.status === 401) {
+                alert('Unauthorized access. [401]');
+            } else if (jqXHR.status === 403) {
+                alert('Forbidden resource can not be accessed. [403]');
+            } else if (jqXHR.status === 404) {
+                alert('Requested page not found. [404]');
+            } else if (jqXHR.status === 500) {
+                alert('Internal server error. [500]');
+            } else if (jqXHR.status === 503) {
+                alert('Service unavailable. [503]');
+            } else if (exception === 'parsererror') {
+                alert('Requested JSON parse failed. [Failed]');
+            } else if (exception === 'timeout') {
+                alert('Time out error. [Timeout]');
+            } else if (exception === 'abort') {
+                alert('Ajax request aborted. [Aborted]');
+            } else {
+                alert('Uncaught Error.n');
+            }
+            console.log("상태: " + status);
+            console.log("실패");
+        }
+    });
+}
+
+
+// 9. writingDelete 글 삭제 시키는 함수
+function writingDelete() {
+
+    $.ajax({
+        type: 'delete'
+        , url: 'https://honeytip.p-e.kr/posts/15'
+        , xhrFields: {
+            withCredentials: false
+        }
+        , success: function (data) {
+            //json 파싱하기
+            let parseData = JSON.parse(data);
+            let datakey = parseData.key;
+            console.log("성공여부" + datakey);
 
         }
         //에러 종류 조건문으로 걸러내기
