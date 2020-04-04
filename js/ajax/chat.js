ce8ae47912f2392058adc3da0c33c45a6a9efe44
writepro4 <writepro4@naver.com>
2020-04-04 11:40:29 +0900

    [update] chat.js 댓글수정 함수 추가

--- js/ajax/chat.js
+++ js/ajax/chat.js
@@ -1,25 +1,21 @@
-// 댓글쓰기
-// (post) https://honeytip.p-e.kr/comments
-//     클라이언트 : , 카테고리 category , 댓글 comment , 유저 이름 userName , 글 번호 postNum
-// 백엔드 : true/false
-//
 // 댓글 수정하기위해 작성했던 댓글 내용가져오기 (edit)
 // (get) https://honeytip.p-e.kr/comments/{댓글 번호}/edit
 //     클라이언트 : x
 // 백엔드 : 댓글 comment
 //
-// 댓글 수정(일부 수정)
-// (PUT [comment]) https://honeytip.p-e.kr/comments/{댓글 번호}
-//     클라이언트 : 댓글 내용 comment
-// 백엔드 : true (트루 폴스 확인 불가능 추후 더 알아보고 수정하겠음)
 //
 // 댓글 삭제
 // (DELETE [comment]) https://honeytip.p-e.kr/comments/{댓글 번호}
 //     클라이언트 : x
 // 백엔드 : true/false (edited)
+// 댓글 목록 (페이징)
+// (get) https://honeytip.p-e.kr/comments/{글 번호}/{댓글 페이징 번호} // 댓글 페이징번호 1번부터 시작
+//     클라이언트 : x
+// 백엔드 : 댓글 comment , 유저 이름 userName , 날짜 date , 글 번호 indexComments , 유저이름 userName
 
 // <--- 함수 리스트 --->
-// 1. getToken 토근 가져오는 함수
+// 1. writeAComment 댓글작성 함수
+// 2. editComment 댓글수정 함수
 
 // 1. 댓글 작성 함수.
 function writeAComment() {
@@ -30,10 +26,10 @@ function writeAComment() {
     const postNum = "0";
 
     let commentData = {
-        'category': token, //이부분에서 '_token'이라는 key로 csrf_token값을 전달해 주어야 한다
-        'comment': adminId,
-        'userName': adminPw,
-        'postNum' : postNum
+        'category': category, //이부분에서 '_token'이라는 key로 csrf_token값을 전달해 주어야 한다
+        'comment': comment,
+        'userName': userName,
+        'postNum': postNum
     };
 
 
@@ -49,10 +45,8 @@ function writeAComment() {
             let parseData = JSON.parse(data);
             let keyCheck = parseData.key;
 
-            console.log("성공여부입니다.: "+keyCheck);
-
-
-
+            // true/false 둘 중 하나를 반환한다.
+            console.log("댓글 작성 성공여부입니다.: " + keyCheck);
 
 
         }
@@ -87,5 +81,58 @@ function writeAComment() {
         }
     });
 
+
+}
+
+// 2. 댓글수정 함수
+function editComment() {
+
+    $.ajax({
+        type: 'post'
+        , url: 'https://honeytip.p-e.kr/comments/0'
+        , data: "댓글내용입니다."
+        , xhrFields: {
+            withCredentials: false
+        }
+        , success: function (data) {
+            //json 파싱하기
+            let parseData = JSON.parse(data);
+            let keyCheck = parseData.key;
+
+            // true/false 둘 중 하나를 반환한다.
+            console.log("댓글 수정 성공여부입니다.: " + keyCheck);
+
+
+        }
+        //에러 종류 조건문으로 걸러내기
+        , error: function (jqXHR, exception) {
+
+            if (jqXHR.status === 0) {
+                alert('Not connect.\n Verify Network.');
+            } else if (jqXHR.status === 400) {
+                alert('Server understood the request, but request content was invalid. [400]');
+            } else if (jqXHR.status === 401) {
+                alert('Unauthorized access. [401]');
+            } else if (jqXHR.status === 403) {
+                alert('Forbidden resource can not be accessed. [403]');
+            } else if (jqXHR.status === 404) {
+                alert('Requested page not found. [404]');
+            } else if (jqXHR.status === 500) {
+                alert('Internal server error. [500]');
+            } else if (jqXHR.status === 503) {
+                alert('Service unavailable. [503]');
+            } else if (exception === 'parsererror') {
+                alert('Requested JSON parse failed. [Failed]');
+            } else if (exception === 'timeout') {
+                alert('Time out error. [Timeout]');
+            } else if (exception === 'abort') {
+                alert('Ajax request aborted. [Aborted]');
+            } else {
+                alert('Uncaught Error.n');
+            }
+            console.log("상태: " + status);
+            console.log("실패");
+        }
+    });
 
 }
