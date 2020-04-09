72c7201821eb54bb1ee0fa9edee32820b4bc8690
writepro4 <writepro4@naver.com>
2020-04-10 03:38:54 +0900

    [update] recent_list.js 쓸모없는 데이터, log 제거

--- js/ajax/recent_list.js
+++ js/ajax/recent_list.js
@@ -10,23 +10,22 @@ $(document).ready(function () {
         }
         , success: function (data) {
 
-            //json 파싱하기
             let parseData = JSON.parse(data);
-            console.log("데이터 파싱전: " + data);
+            // console.log("데이터 파싱전: " + data);
             let keyCheck = parseData.key;
 
-            console.log("데이터 성공여부 : " + keyCheck);
+            // console.log("데이터 성공여부 : " + keyCheck);
 
             // 요청값이 true일 경우에 실행
             if (keyCheck === true) {
 
                 let postInfoData = parseData.postInfo;
-                console.log(postInfoData);
+                // console.log("파싱된 데이터 : " + postInfoData);
 
                 // key값도 배열에 포함되기 때문에 -1로 값을 맞춰줌
                 let postingListData = postInfoData.length;
 
-                console.log("받아온 데이터 확인: " + postingListData);
+                // console.log("받아온 데이터 확인: " + postingListData);
 
 
                 for (let i = 0; i < postingListData; i++) {
@@ -42,11 +41,11 @@ $(document).ready(function () {
                     let category = postInfoData[i].category;
                     let date = postInfoData[i].date;
 
-                    let categoryPostsID = category+"_"+indexPosts;
+                    let categoryPostsID = category + "_" + indexPosts;
 
-                    console.log(categoryPostsID);
-
-                    console.log("아이디 값입니다.:" + indexPosts);
+                    // console.log(categoryPostsID);
+                    //
+                    // console.log("아이디 값입니다.:" + indexPosts);
 
                     let html = `<div class="column" onclick="detailPage(this)" id=${indexPosts}>`;
                     html += `<div class="ui card">`;
@@ -76,10 +75,6 @@ $(document).ready(function () {
                     cardView += `</div>`;
                     cardView += `</div>`;
                     cardView += `<div class="ui two bottom attached buttons">`;
-                    // cardView += `<div class="ui button">`;
-                    // cardView += `<i class="add icon"></i>`;
-                    // cardView += `Queue`;
-                    // cardView += `</div>`;
                     cardView += `<div class="ui white button">`;
                     cardView += `<i class="play icon"></i>`;
                     cardView += `글 보기 `;
@@ -92,15 +87,15 @@ $(document).ready(function () {
                     cardView += `</div>`;
 
 
-                    console.log("제목" + title);
-                    console.log("날짜" + date);
-                    console.log("타이틀 이미지" + titleImage);
-                    console.log("글 아이디" + indexPosts);
-                    console.log("관리자 아이디" + adminId);
-                    console.log("글 내용" + contents);
-                    console.log("조회수" + viewCount);
-                    console.log("카테고리 종류" + category);
-                    console.log("서브 제목" + subTitle);
+                    // console.log("제목" + title);
+                    // console.log("날짜" + date);
+                    // console.log("타이틀 이미지" + titleImage);
+                    // console.log("글 아이디" + indexPosts);
+                    // console.log("관리자 아이디" + adminId);
+                    // console.log("글 내용" + contents);
+                    // console.log("조회수" + viewCount);
+                    // console.log("카테고리 종류" + category);
+                    // console.log("서브 제목" + subTitle);
 
                     $('#grid').append(cardView);
                     $('#detailGrid').append(cardView);
