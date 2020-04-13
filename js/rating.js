dfbc7c2dad47ef42a1619cc3297dd4522512ee5b
writepro4 <writepro4@naver.com>
2020-04-13 14:22:01 +0900

    [update] rating.js 코드 변경

--- js/rating.js
+++ js/rating.js
@@ -1,15 +1,7 @@
 //semantic ui rating 함수
 $(document).ready(function(){
 
-    //retrieve the data from server
-    let ratingNumber = 2;
-
-    //Initilize the rating and get the rate
-    $('.ui.rating').rating({
-        initialRating: ratingNumber,
-        maxRating: 5,
-        onRate: function(rating){
-            console.log(rating);
-        }
-    });
+    $('.ui.rating')
+        .rating()
+    ;
 });
