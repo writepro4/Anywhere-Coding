e83ed2d1d521e09f618c71141f4d5eb49a3fb560
writepro4 <writepro4@naver.com>
2020-04-13 10:02:11 +0900

    [update] chat.js 대댓글함수 추가

--- js/ajax/chat.js
+++ js/ajax/chat.js
@@ -6,6 +6,7 @@
 // 5. 글번호 가져오는 함수
 // 6. 카테고리 가져오는 함수
 // 7. 댓글수정 창 호출하는 함수
+// 8. largeComment 대댓글 작성 함수
 
 
 // 1. 댓글 작성 함수.
@@ -272,10 +273,6 @@ function loadingComments() {
                 let listOfComments = contents.length;
 
 
-                let parse = parseFloat(contents);
-                console.log(parse);
-
-
                 let chatTitle = `<h3 class="ui dividing header"><span style="vertical-align: inherit;"><span`;
                 chatTitle += `style="vertical-align: inherit;"><i class="comment icon"></i>댓글 </span></span></h3>`;
                 chatTitle += `<h3 class="ui dividing header reply center aligned"><span style="font-size: 16px; color:rgba(0, 0, 0, 0.8); ">이전 댓글보기</span></h3>`;
@@ -447,6 +444,102 @@ function editCommentWindow(crystalID) {
     formData += `</form>`;
 
     $(`#${editCommentID}`).append(formData);
+
+
+}
+
+
+// 8. largeComment 대댓글 작성 함수
+function largeComment() {
+    const postNum = getArticleNumber();
+    const categoryUrl = categoryImport();
+
+    console.log("글번호: " + postNum);
+    console.log("카테고리 번호 : " + categoryUrl);
+
+    const comment = $('#replyForm').val();
+    console.log(comment);
+    const userName = "민차뉘";
+
+    let commentData = {
+        'category': categoryUrl, //이부분에서 '_token'이라는 key로 csrf_token값을 전달해 주어야 한다
+        'comment': comment,
+        'userName': userName,
+        'postNum': postNum
+    };
+
+
+    $.ajax({
+        type: 'post'
+        , url: 'https://honeytip.p-e.kr/comments'
+        , data: commentData
+        , xhrFields: {
+            withCredentials: false
+        }
+        , success: function (data) {
+            //json 파싱하기
+            let parseData = JSON.parse(data);
+            let keyCheck = parseData.key;
+            const commentID = parseData.commentIndex;
+
+            console.log("반환 데이터 확인 : " + data);
+            // true/false 둘 중 하나를 반환한다.
+            console.log("댓글 작성 성공여부입니다.: " + keyCheck);
+
+            let chatData = `<div class="comments">`;
+            chatData += `<div class="comment">`;
+            chatData += `<a class="avatar">`;
+            chatData += `<img src="/images/avatar/small/jenny.jpg">`;
+            chatData += `</a>`;
+            chatData += `<div class="content">`;
+            chatData += `<a class="author">Jenny Hess</a>`;
+            chatData += `<div class="metadata">`;
+            chatData += `<span class="date">Just now</span>`;
+            chatData += `</div>`;
+            chatData += `<div class="text">`;
+            chatData += `Elliot you are always so right :)`;
+            chatData += `</div>`;
+            chatData += `<div class="actions">`;
+            chatData += `<a class="reply">Reply</a>`;
+            chatData += `</div>`;
+            chatData += `</div>`;
+            chatData += `</div>`;
+            chatData += `</div>`;
+
+            $('#comments').append(chatData);
+
+
+        }
+        //에러 종류 조건문으로 걸러내기
+        , error: function (jqXHR, exception) {
+
+            if (jqXHR.status === 0) {
+                alert('Not connect.\n Verify Network.');
+            } else if (jqXHR.status === 400) {
+                alert('Server understood the request, but request content was invalid. [400]');
+            } else if (jqXHR.status === 401) {
+                alert('Unauthorized access. [401]');
+            } else if (jqXHR.status === 403) {
+                alert('Forbidden resource can not be accessed. [403]');
+            } else if (jqXHR.status === 404) {
+                alert('Requested page not found. [404]');
+            } else if (jqXHR.status === 500) {
+                alert('Internal server error. [500]');
+            } else if (jqXHR.status === 503) {
+                alert('Service unavailable. [503]');
+            } else if (exception === 'parsererror') {
+                alert('Requested JSON parse failed. [Failed]');
+            } else if (exception === 'timeout') {
+                alert('Time out error. [Timeout]');
+            } else if (exception === 'abort') {
+                alert('Ajax request aborted. [Aborted]');
+            } else {
+                alert('Uncaught Error.n');
+            }
+            console.log("상태: " + status);
+            console.log("실패");
+        }
+    });
 
 
 }
