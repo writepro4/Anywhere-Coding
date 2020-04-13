c80eef622b65954961ce50b50a0fa76459ba6ab8
writepro4 <writepro4@naver.com>
2020-04-13 16:09:57 +0900

    [update] ajax.js 불필요한 주석제거, 불필요한 함수 모두 정리

--- js/ajax/ajax.js
+++ js/ajax/ajax.js
@@ -1,13 +1,7 @@
-// 변수명은 메소드명은 낙타로 통일하기
-
 // <--- 함수 리스트 --->
 // 1. getToken 토근 가져오는 함수
-// 2. writinglist 글 상세 내용 불러오는 함수
-// 3. categoryList 카테고리 목록 불어오는 함수
-// 4. categoryCheck카테고리 선택시 해당 카테고리로 이동하는 함수
-// 5. ajax로 데이터를 가져오고 페이지 갱신없이 주소 변경하는 함수.
-// 6. 다음 페이지에 데이터 넘기는 함수.
-// 7. 상세페이지에 데이터 넘기는 함수.
+// 2. nextPageData 카테고리 페이지에 데이터 넘기는 함수.
+// 3. detailPage 상세페이지에 데이터 넘기는 함수.
 
 
 // 1. 토근 가져오는 함수
@@ -37,168 +31,7 @@ function getToken(callback) {
     });
 }
 
-
-// 2. 글 상세 내용 불러오는 함수
-function writinglist() {
-
-    $.ajax({
-        type: 'get'
-        , url: 'https://honeytip.p-e.kr/posts/8'
-        , xhrFields: {
-            withCredentials: false
-        }
-        , success: function (data) {
-            //json 파싱하기
-            let parseData = JSON.parse(data);
-            let keyCheck = parseData.key;
-
-            let postInfo = parseData.postInfo;
-
-            let title = postInfo.title;
-            let content = postInfo.contents;
-            let date = postInfo.date;
-            let viewCounts = postInfo.viewCounts;
-
-            console.log("key" + keyCheck);
-            console.log("제목" + title);
-            console.log("내용" + content);
-            console.log("날짜" + date);
-            console.log("본 횃수" + viewCounts);
-
-
-        }
-        //에러 종류 조건문으로 걸러내기
-        , error: function (jqXHR, exception) {
-
-            if (jqXHR.status === 0) {
-                alert('Not connect.\n Verify Network.');
-            } else if (jqXHR.status === 400) {
-                alert('Server understood the request, but request content was invalid. [400]');
-            } else if (jqXHR.status === 401) {
-                alert('Unauthorized access. [401]');
-            } else if (jqXHR.status === 403) {
-                alert('Forbidden resource can not be accessed. [403]');
-            } else if (jqXHR.status === 404) {
-                alert('Requested page not found. [404]');
-            } else if (jqXHR.status === 500) {
-                alert('Internal server error. [500]');
-            } else if (jqXHR.status === 503) {
-                alert('Service unavailable. [503]');
-            } else if (exception === 'parsererror') {
-                alert('Requested JSON parse failed. [Failed]');
-            } else if (exception === 'timeout') {
-                alert('Time out error. [Timeout]');
-            } else if (exception === 'abort') {
-                alert('Ajax request aborted. [Aborted]');
-            } else {
-                alert('Uncaught Error.n');
-            }
-            console.log("상태: " + status);
-            console.log("실패");
-        }
-    });
-}
-
-
-// 3. categoryList 카테고리 목록 불어오는 함수
-function categoryList() {
-
-    const categoryNumber = location.href.substr(
-        location.href.lastIndexOf('=') + 1
-    );
-
-
-    $.ajax({
-        type: 'get'
-        , url: 'https://honeytip.p-e.kr/posts/' + categoryNumber + '/1'
-        , xhrFields: {
-            withCredentials: false
-        }
-        , success: function (data) {
-
-            //json 파싱하기
-            let parseData = JSON.parse(data);
-            console.log("데이터 파싱전: " + data);
-            let keyCheck = parseData.key;
-            console.log("데이터 성공여부 : " + keyCheck);
-
-            let postInfoData = parseData.contents;
-
-            let postingListData = postInfoData.length;
-
-            console.log("받아온 데이터 확인: " + postingListData);
-
-            for (let i = 0; i < postingListData; i++) {
-
-
-                let title = postInfoData[i].title;
-                let titleImage = postInfoData[i].image;
-                let subTitle = postInfoData[i].sub_title;
-                let indexPosts = postInfoData[i].index_posts;
-                let adminId = postInfoData[i].admin_id;
-                let contents = postInfoData[i].contents;
-                let viewCount = postInfoData[i].view_count;
-                let category = postInfoData[i].category;
-                let date = postInfoData[i].date;
-
-                console.log("제목" + title);
-                console.log("날짜" + date);
-                console.log("타이틀 이미지" + titleImage);
-                console.log("글 아이디" + indexPosts);
-                console.log("관리자 아이디" + adminId);
-                console.log("글 내용" + contents);
-                console.log("조회수" + viewCount);
-                console.log("카테고리 종류" + category);
-                console.log("서브 제목" + subTitle);
-
-            }
-
-        }
-        //에러 종류 조건문으로 걸러내기
-        , error: function (jqXHR, exception) {
-
-            if (jqXHR.status === 0) {
-                alert('Not connect.\n Verify Network.');
-            } else if (jqXHR.status === 400) {
-                alert('Server understood the request, but request content was invalid. [400]');
-            } else if (jqXHR.status === 401) {
-                alert('Unauthorized access. [401]');
-            } else if (jqXHR.status === 403) {
-                alert('Forbidden resource can not be accessed. [403]');
-            } else if (jqXHR.status === 404) {
-                alert('Requested page not found. [404]');
-            } else if (jqXHR.status === 500) {
-                alert('Internal server error. [500]');
-            } else if (jqXHR.status === 503) {
-                alert('Service unavailable. [503]');
-            } else if (exception === 'parsererror') {
-                alert('Requested JSON parse failed. [Failed]');
-            } else if (exception === 'timeout') {
-                alert('Time out error. [Timeout]');
-            } else if (exception === 'abort') {
-                alert('Ajax request aborted. [Aborted]');
-            } else {
-                alert('Uncaught Error.n');
-            }
-            console.log("상태: " + status);
-            console.log("실패");
-        }
-    });
-}
-
-
-// 4. 카테고리 선택시 해당 카테고리로 이동하는 함수
-function categoryCheck(category) {
-    let categoryData = category.id;
-    console.log(categoryData);
-
-    console.log(getCookie("category"));
-
-    window.location.replace("./category_page.html");
-}
-
-
-// 5. 카테고리 페이지에 데이터 넘기는 함수.
+// 2. 카테고리 페이지에 데이터 넘기는 함수.
 function nextPageData(category) {
     let categoryData = category.id;
     //데이터 넘기는지 확인용도
@@ -207,7 +40,7 @@ function nextPageData(category) {
 
 }
 
-// 6. 상세페이지에 데이터 넘기는 함수.
+// 3. 상세페이지에 데이터 넘기는 함수.
 function detailPage(page) {
     let detailPage = page.id;
     console.log(detailPage);
@@ -215,14 +48,7 @@ function detailPage(page) {
     window.location.href = `./details_page.html?index=${detailPage}`;
 }
 
-// 7. url데이터  가져오는 함수
-$(document).ready(function () {
-    const categoryNumber = location.href.substr(
-        location.href.lastIndexOf('=') + 1
-    );
 
-    console.log('val : ' + categoryNumber);
-});
 
 
 
