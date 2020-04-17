2bc175fcc414c0d2a716509df11841ac1fa58a51
writepro4 <writepro4@naver.com>
2020-04-17 14:33:42 +0900

    [fix] nav.js 세션스토리지에서 정보를 가져와 로그인 유무판단하게 변경

--- js/nav.js
+++ js/nav.js
@@ -2,9 +2,25 @@
 
 $(document).ready(function () {
 
-    $("#header").load("header.html");
+    let loginCheck = sessionStorageGet('login');
+
+    if (loginCheck === "로그인") {
+        console.log("가져온 값 : "+sessionStorageGet(`login`));
+        $("#header").load("headerLogin.html");
+    } else {
+        console.log("가져온 값 : "+sessionStorageGet(`login`));
+        console.log("값 못가져옴");
+        $("#header").load("header.html");
+
+    }
     $("#footer").load("footer.html");
 
     //~이렇게 한줄만 해주면 알아서 contents에 testContents.html파일을 넣어 준다.
 
 });
+
+
+//7. sessionStorageGet 세션스토리지에 값 가져오는 함수.
+function sessionStorageGet(key) {
+    return window.sessionStorage.getItem(key);
+}
