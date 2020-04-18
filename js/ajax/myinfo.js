8b000cfb8debff8edb8f3588b2acd8c9e900072b
writepro4 <writepro4@naver.com>
2020-04-18 14:53:09 +0900

    [fix] myinfo.js 유저 프로필카드 출력

--- js/ajax/myinfo.js
+++ js/ajax/myinfo.js
@@ -3,20 +3,17 @@
 // 2. withdrawal 회원탈퇴 함수.
 // 3. localStorageGet 로컬스토리지 값 가져오는 함수.
 
-//TODO 로그인
-
 $(document).ready(function () {
-    // 유저정보페이지
-    // (get) https://honeytip.p-e.kr/user/{uid}
-    //     클라이언트 :  uid (구글회원번호 로그인시 전달해주는 값)
-    // 백엔드 : false / true 유저 이름(name), 유저 프로필사진 (avatar) , [나중에] 뱃지(badge)
 
     let name = localStorageGet('name');
     let avatar = localStorageGet('avatar');
 
+    const uid = sessionStorageGet('cf');
+
+    console.log("uid확인 : " + uid);
     $.ajax({
         type: 'get'
-        , url: `https://honeytip.p-e.kr/user/{uid}`
+        , url: `https://honeytip.p-e.kr/user/${uid}`
         , xhrFields: {
             withCredentials: false
         }
@@ -24,7 +21,35 @@ $(document).ready(function () {
             //json 파싱하기
             let parseData = JSON.parse(data);
             let keyCheck = parseData.key;
+            console.log("데이터 확인 : " + data);
+            let userInfo = parseData.userInfo;
+            let name = userInfo[0].name;
+            let image = userInfo[0].avatar;
+            let badge = userInfo[0].badge;
 
+            console.log("유저 이름 : " + name);
+            console.log("유저 이미지 : " + image);
+            console.log("유저 뱃지 : " + badge);
+
+            let cardView = `<div class="ui card">`;
+            cardView += `<div class="image">`;
+            cardView += `<img src=${image}>`;
+            cardView += `</div>`;
+            cardView += `<div class="content">`;
+            cardView += `<a class="header">${name}</a>`;
+            cardView += `<div class="meta">`;
+            cardView += `<span class="date">반갑습니다.</span>`;
+            cardView += `</div>`;
+            cardView += `</div>`;
+            cardView += `<div class="extra content">`;
+            // cardView += `<a>`;
+            // cardView += `<i class="user icon"></i>`;
+            // cardView += `22 Friends`;
+            // cardView += `</a>`;
+            cardView += `</div>`;
+            cardView += `</div>`;
+
+            $('#cardProfile').append(cardView);
 
 
         }
@@ -61,9 +86,8 @@ $(document).ready(function () {
 
 });
 
-
 //1.내 정보 가져오는 함수.
-function getMyInformation(){
+function getMyInformation() {
 
     // 유저정보페이지
     // (get) https://honeytip.p-e.kr/user/{uid}
@@ -81,7 +105,6 @@ function getMyInformation(){
             //json 파싱하기
             let parseData = JSON.parse(data);
             let keyCheck = parseData.key;
-
 
 
         }
@@ -118,7 +141,7 @@ function getMyInformation(){
 }
 
 //2. 회원탈퇴 함수
-function withdrawal(){
+function withdrawal() {
 
 // 회원탈퇴
 // (DELETE) https://honeytip.p-e.kr/user/{uid}
@@ -135,7 +158,6 @@ function withdrawal(){
             //json 파싱하기
             let parseData = JSON.parse(data);
             let keyCheck = parseData.key;
-
 
 
         }
