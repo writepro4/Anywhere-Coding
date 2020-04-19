fb4793a233c55c37b67f88d4b18df69efa6451e7
writepro4 <writepro4@naver.com>
2020-04-19 10:03:54 +0900

    [fix] login.js 메인페이지에서 footer메뉴바 표시안되던 문제 수정

--- js/ajax/login.js
+++ js/ajax/login.js
@@ -35,20 +35,25 @@ $(document).ready(function () {
     if (confirmId === `https://honeytip.kro.kr/` && sessionStorageGet('cf') === null) {
         console.log("로그인 안된상태.");
         $("#header").load("header.html");
+        $("#footer").load("footer.html");
     } else if (confirmId === `https://honeytip.kro.kr/index.html` && sessionStorageGet(`cf`) === null) {
         console.log("로그인 안된상태");
         $("#header").load("header.html");
+        $("#footer").load("footer.html");
     } else if (confirmId === `https://honeytip.kro.kr/` && sessionStorageGet('cf') !== null) {
         console.log("로그인 로컬에 저장된 상태");
         $("#header").load("headerLogin.html");
+        $("#footer").load("footer.html");
         loginRequest();
     } else if (confirmId === `https://honeytip.kro.kr/index.html` && sessionStorageGet(`cf`) !== null) {
         console.log("로그인 로컬에 저장된 상태");
         $("#header").load("headerLogin.html");
+        $("#footer").load("footer.html");
         loginRequest();
     } else {
         sessionStorageSet('cf', confirmId);
         $("#header").load("headerLogin.html");
+        $("#footer").load("footer.html");
         loginRequest();
     }
 
