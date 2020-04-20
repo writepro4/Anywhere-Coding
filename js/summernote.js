8e79896ab33e83bc6d09e31c98d4ed00357f43df
writepro4 <writepro4@naver.com>
2020-04-20 19:03:52 +0900

    [update] administrator_page.html 썸머노트 가로 크기 수정

--- js/summernote.js
+++ js/summernote.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $('#summernote').summernote({
-        height: 1000,                 // 에디터 높이
+        height: 1000,
+        width: 1500,// 에디터 높이
         minHeight: null,             // 최소 높이
         maxHeight: null,             // 최대 높이
         focus: true,                  // 에디터 로딩후 포커스를 맞출지 여부
