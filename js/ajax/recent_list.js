6f2526a2e4630a7b371622cdd2540ea03bc73b6f
writepro4 <writepro4@naver.com>
2020-04-21 12:57:25 +0900

    [update] recent_list.js 최근카테고리 카드뷰 글꼴 변경

--- js/ajax/recent_list.js
+++ js/ajax/recent_list.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
                     let categoryPostsID = category + "_" + indexPosts;
 
 
-                    let cardView = `<a class="ui card" onclick="detailPage(this)" id=${categoryPostsID} style="font-style:italic">`;
+                    let cardView = `<a class="ui card" onclick="detailPage(this)" id=${categoryPostsID}>`;
                     cardView += `<div class="image">`;
                     cardView += `<img src=${titleImage} style="height: 120px;">`;
                     cardView += `</div>`;
