5ce78c68ab216ee2dd1532ca37d98b753e4d887f
writepro4 <writepro4@naver.com>
2020-04-22 10:13:04 +0900

    [fix] summernote.js 써머노트 js파일 코드 수정

--- js/summernote.js
+++ js/summernote.js
@@ -1,15 +1,11 @@
 $(document).ready(function () {
     $('#summernote').summernote({
-        height: 1000,
-        width: 1500,// 에디터 높이
+        height: 1000,                 // 에디터 높이
         minHeight: null,             // 최소 높이
         maxHeight: null,             // 최대 높이
         focus: true,                  // 에디터 로딩후 포커스를 맞출지 여부
         lang: "ko-KR",					// 한글 설정
-        placeholder: '최대 2048자까지 쓸 수 있습니다',
-        disableDragAndDrop: true,
-        spellCheck: false
-
+        placeholder: '최대 2048자까지 쓸 수 있습니다'	//placeholder 설정
 
     });
 });
