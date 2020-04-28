22cdb80e34bada53d99a94d2cdc72237c05494be
writepro4 <writepro4@naver.com>
2020-04-28 13:27:08 +0900

    [update] commentManage.js 댓글관리창 페이징처리 완료

--- js/ajax/commentManage.js
+++ js/ajax/commentManage.js
@@ -1,7 +1,8 @@
-//작성된 댓글 목록 불러오는 함수.
+//1. 작성된 댓글 목록 불러오는 함수.
+//2. pageingComments() 댓글 목록 페이징 함수.
 
 
-//1.내 정보 가져오는 함수.
+//1.댓글 목록 가져오는 함수.
 $(document).ready(function () {
     $.ajax({
         type: 'get'
@@ -12,15 +13,33 @@ $(document).ready(function () {
         }
         , success: function (data) {
             //json 파싱하기
-            console.log("데이터 확인 : "+ data);
+            console.log("데이터 확인 : " + data);
             const parseData = JSON.parse(data);
             const keyCheck = parseData.key;
+            const pageCount = parseData.pageCount;
+            console.log(pageCount);
 
             const {contents} = parseData;
-            // console.table(contents);
             console.table(contents);
 
             let CommentList = contents.length;
+
+
+            for (let i = 0; i < pageCount; i++) {
+                console.log("페이징 버튼 생성중");
+
+                const pageingButton = `<a class="item" onclick="pageingComments(this)" id=${i + 1}>
+                    ${i + 1}
+                    </a>`;
+
+                console.log("데이터 확인 : " + pageingButton);
+
+
+                $(`#pageing`).append(pageingButton);
+
+
+            }
+
 
             for (let i = 0; i < CommentList; i++) {
 
@@ -90,3 +109,125 @@ $(document).ready(function () {
     });
 
 });
+
+
+// 2. pageingComments() 댓글 목록 페이징 함수.
+function pageingComments(pageNumber) {
+
+    const pageing = pageNumber.id;
+
+    $(`#commentManagement`).remove();
+
+    const pageingwindow =  `<div id="commentManagement">
+        <div class="ui relaxed list " id="listOfComments">
+        </div>
+        <div class="ui pagination menu" id="pageing">
+        </div>
+        </div>`;
+
+    $(`#window`).append(pageingwindow);
+
+
+
+    $.ajax({
+        type: 'get'
+        , url: `https://honeytip.p-e.kr/comments/${pageing}`
+        , xhrFields: {
+            withCredentials: false
+        }
+        , success: function (data) {
+            //json 파싱하기
+            console.log("데이터 확인 : " + data);
+            const parseData = JSON.parse(data);
+            const keyCheck = parseData.key;
+            const pageCount = parseData.pageCount;
+            console.log(pageCount);
+
+            const {contents} = parseData;
+            // console.table(contents);
+            console.table(contents);
+
+            let CommentList = contents.length;
+
+
+            for (let i = 0; i < pageCount; i++) {
+                console.log("페이징 처리 중 ");
+
+                const pageing = `<a class="item" onclick="pageingComments(this)" id=${i+1}>
+                    ${i + 1}
+                    </a>`;
+
+                $(`#pageing`).append(pageing);
+
+
+            }
+
+
+            for (let i = 0; i < CommentList; i++) {
+
+                const {userName} = contents[i];
+                const {category} = contents[i];
+                const {date} = contents[i];
+                const {indexComments} = contents[i];
+                const {comment} = contents[i];
+                const {postNum} = contents[i];
+
+                const categoryPost = category + `_` + postNum;
+
+                const chatData = `<div class="item ui segment" id=${indexComments}>
+                <div class="right floated content">
+                <div class="ui red button" onclick="deleteComment(this)" id=${indexComments}>삭제</div>
+                <div class="ui olive button" onclick="detailPage(this)" id=${categoryPost}>페이지로 이동</div>
+                <div class="content">작성날짜 : ${date}</div>
+<!--                <div class="ui blue button" onclick="writingFix()">수정</div>-->
+                </div>
+                <div class="header">작성자 : ${userName}</div>
+                <div class="content">
+                내용 : ${comment}
+                </div>
+                <br>
+                <div class="content">
+                <i class="pencil icon"></i>카테고리 : ${category}
+                </div>
+                </div>`;
+
+
+                $(`#listOfComments`).append(chatData);
+
+
+            }
+
+
+        }
+        //에러 종류 조건문으로 걸러내기
+        , error: function (jqXHR, exception) {
+
+            if (jqXHR.status === 0) {
+                alert('Not connect.\n Verify Network.');
+            } else if (jqXHR.status === 400) {
+                alert('Server understood the request, but request content was invalid. [400]');
+            } else if (jqXHR.status === 401) {
+                alert('Unauthorized access. [401]');
+            } else if (jqXHR.status === 403) {
+                alert('Forbidden resource can not be accessed. [403]');
+            } else if (jqXHR.status === 404) {
+                alert('Requested page not found. [404]');
+            } else if (jqXHR.status === 500) {
+                alert('Internal server error. [500]');
+            } else if (jqXHR.status === 503) {
+                alert('Service unavailable. [503]');
+            } else if (exception === 'parsererror') {
+                alert('Requested JSON parse failed. [Failed]');
+            } else if (exception === 'timeout') {
+                alert('Time out error. [Timeout]');
+            } else if (exception === 'abort') {
+                alert('Ajax request aborted. [Aborted]');
+            } else {
+                alert('Uncaught Error.n');
+            }
+            console.log("상태: " + status);
+            console.log("실패");
+        }
+    });
+
+}
